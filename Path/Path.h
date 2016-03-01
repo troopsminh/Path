fda80a9c25da0411c1d292f92ce11b1fcb424b57
christian otkjær <christian.otkjaer@gmail.com>
2016-03-01 15:34:56 +0100

    Initial Commit

---
+++ Path/Path.h
@@ -0,0 +1,19 @@
+//
+//  Path.h
+//  Path
+//
+//  Created by Christian Otkjær on 01/03/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Path.
+FOUNDATION_EXPORT double PathVersionNumber;
+
+//! Project version string for Path.
+FOUNDATION_EXPORT const unsigned char PathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Path/PublicHeader.h>
+
+
